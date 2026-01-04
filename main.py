bd4c83bf0837e2531185c4683418b290dfe9d81d
usr-Ayan <ayanry2050@gmail.com>
2026-01-05 00:52:28 +0530

    Data Ingestion stage completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("welcome to custom logging")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,10 @@
+#update entity it is nothing but return type of function
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
